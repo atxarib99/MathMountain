05e308c6bd93024567dd5f97ab87abca19f69887
Arib Dhuka <atxarib99@gmail.com>
2016-07-13 19:41:22 -0500

    Added Music

--- app/src/main/java/com/example/arib/mathmountain/GameSelectionActivity.java
+++ app/src/main/java/com/example/arib/mathmountain/GameSelectionActivity.java
@@ -1,15 +1,20 @@
 package com.example.arib.mathmountain;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class GameSelectionActivity extends AppCompatActivity {
 
+    MediaPlayer song;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        song = MediaPlayer.create(this, R.raw.song);
+        song.setLooping(true);
+        song.start();
         setContentView(R.layout.activity_game_selection);
     }
 
